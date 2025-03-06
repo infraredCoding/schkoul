be7c9f64a2f8ad3b1072477aee09d3f0c6e79fcd
Imran Rahman <imranspeedcuber@gmail.com>
2025-03-06 20:25:40 +0600

    build for frontend

--- client/src/axios/index.js
+++ client/src/axios/index.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: 'http://127.0.0.1:8080/api/v1',
-  // baseURL: 'https://schkoul.imraninfrared.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
